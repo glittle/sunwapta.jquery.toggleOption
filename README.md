af6ea888120cb310bed1075ee93b7d86e7336f14
Glen Little <glittle@sunwaptasolutions.com>
2014-01-17 15:26:50 -0700

    1.0.0

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-sunwapta.jquery.toggleOption
-============================
+# Toggle Dropdown Options
 
-jQuery plugin that hides and shows &lt;options> in a &lt;select> element
+## Description
+
+A jQuery plugin that hides and shows &lt;options> in a &lt;select> element
+
+## Usage
+
+Sample HTML:
+
+    &lt;select id="colors">
+      &lt;option>Choose a color...&lt;/option>
+      &lt;option value="red">Red&lt;/option>
+      &lt;option value="green">Green&lt;/option>
+      &lt;option value="blue">Blue&lt;/option>
+    &lt;/select>
+
+Javascript:
+
+    $('#colors').hideOption('green');
---
+++ sunwapta.toggleOption.js
@@ -0,0 +1,64 @@
+(function ($) {
+  $.fn.toggleOption = function (value, show) {
+    /// <summary>Show or hide the desired option</summary>
+    return this.filter('select').each(function () {
+      if (show) {
+        $(this).showOption(value);
+      }
+      else {
+        $(this).hideOption(value);
+      }
+    });
+  };
+  $.fn.showOption = function (value) {
+    /// <summary>Show the desired option in the location it was in when hideOption was first used</summary>
+    return this.filter('select').each(function () {
+      var select = $(this);
+      var testOpt = select.find('option[value="' + value + '"]').eq(0);
+      if (testOpt.length) return; // already there
+
+      var info = select.data('opt' + value);
+      if (!info) return; // abort... hideOption has not been used yet
+
+      var targetIndex = info.data('i');
+      var options = select.find('option');
+      var lastIndex = options.length - 1;
+      if (lastIndex == -1) {
+        select.prepend(info);
+      }
+      else {
+        options.each(function (i, e) {
+          var opt = $(e);
+          if (opt.data('i') > targetIndex) {
+            opt.before(info);
+            return false;
+          }
+          else if (i == lastIndex) {
+            opt.after(info);
+            return false;
+          }
+        });
+      }
+      return;
+    });
+  };
+  $.fn.hideOption = function (value) {
+    /// <summary>Hide the desired option, but remember where it was to be able to put it back where it was</summary>
+    return this.filter('select').each(function () {
+      var select = $(this);
+      var opt = select.find('option[value="' + value + '"]').eq(0);
+      if (!opt.length) return;
+
+      if (!select.data('optionsModified')) {
+        // remember the order
+        select.find('option').each(function (i, e) {
+          $(e).data('i', i);
+        });
+        select.data('optionsModified', true);
+      }
+
+      select.data('opt' + value, opt.detach());
+      return;
+    });
+  };
+})(jQuery);
